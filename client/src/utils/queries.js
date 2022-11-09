004eb9c2395146785dc53654d8a930c29f893bae
unknown <tetiana.lisina@gmail.comgit config --global user.name T8886>
2022-11-09 13:06:14 -0600

    Login/signup working

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,6 +6,7 @@ export const QUERY_USER = gql`
       _id
       username
       email
+      sign
       thoughts {
         _id
         thoughtText
@@ -49,6 +50,7 @@ export const QUERY_ME = gql`
       _id
       username
       email
+      sign
       thoughts {
         _id
         thoughtText
