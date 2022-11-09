74291ed0caa6f5671bbccc8d2f8b60fba66be6f2
unknown <tetiana.lisina@gmail.comgit config --global user.name T8886>
2022-11-09 11:33:01 -0600

    finished reviewing, adding code

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -7,15 +7,19 @@ class AuthService {
 
   loggedIn() {
     const token = this.getToken();
+    // If there is a token and it's not expired, return `true`
     return token && !this.isTokenExpired(token) ? true : false;
   }
 
   isTokenExpired(token) {
+    // Decode the token to get its expiration time that was set by the server
     const decoded = decode(token);
+    // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
     if (decoded.exp < Date.now() / 1000) {
       localStorage.removeItem('id_token');
       return true;
     }
+    // If token hasn't passed its expiration time, return `false`
     return false;
   }
 
