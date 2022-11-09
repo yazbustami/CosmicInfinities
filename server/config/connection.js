2f8be06252d82f7afd00b9eb549a9e07e34d7208
Yasmin <bustamiyasmin@gmail.com>
2022-11-09 14:26:46 -0600

    adding db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/CI2',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
