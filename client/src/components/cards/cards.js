64cd65ec02f6e062e201af8f2e3084a4932adde7
T8886 <tetiana.lisina@gmail.comgit config --global user.name T8886>
2022-11-11 12:01:14 -0600

    added bootstrap, cards

---
+++ client/src/components/cards/cards.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// function GridExample() {
+//   return (
+// <div class="card" style="width: 18rem;">
+//   <img src="..." class="card-img-top" alt="..." />
+//   <div class="card-body">
+//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//   </div>
+// </div>
+//   );
+// }
+
+// export default GridExample;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
+// import {GridExample} from "../components/cards/cards.js";
+
 import Auth from '../utils/auth';
 // import "../pages/Styles/SU.css"
 // import galaxy from "../pages/Assets/Pictures/galaxy1.jpeg"
@@ -105,8 +107,50 @@ const Signup = () => {
           </div>
         </div>
       </div>
+      {/* <GridExample /> */}
+
+      <div className="row row-cols-1 row-cols-md-2">
+  <div className="col mb-4">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col mb-4">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col mb-4">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col mb-4">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
     </main>
   );
 };
 
 export default Signup;
+
