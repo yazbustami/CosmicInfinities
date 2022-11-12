d7f440839aeb4cf51a0da730df2af17d373fb8bf
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-12 15:02:37 -0500

    override reboot.scss

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import constellation from '../../pages/Assets/Pictures/favicon-32x32.png'
-
+// import constellation from '../../pages/Assets/Pictures/favicon-32x32.png'
 import Auth from '../../utils/auth';
+import logo from '../../pages/Assets/Pictures/cosmicLogo.png'
+
+
+
 
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+
+  const headerStyle = {
+    color: "Black",
+    backgroundColor: '#f5eaee;',
+    padding: "10px",
+  };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <header className="text-light mb-4 py-3 flex-row align-center" style={{headerStyle}}>
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0"> <a href="/">
-              C<img src={constellation} width="25px" alt=""/>smic Infinities </a> </h1>
+            {/* <h1 className="m-0"> <a href="/">
+              C<img src={constellation} width="25px" alt=""/>smic Infinities </a> </h1> */}
+               <img style={{width: 550, height: 200, alignSelf: 'center' }}src={logo} alt="logo" />
           </Link>
-          <p className="m-0">Look at what's written in the stars...</p>
+
         </div>
         <div>
           {Auth.loggedIn() ? (
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import zodiac from './Assets/Pictures/zodiacsigns.png';
+import stars from './Assets/Pictures/starPattern1.png';
+import '../'
 
 const Landing = () => {
-return( 
-    <h1>hello world</h1>
-)
+  return (
+    <div className="container" id='landing-container'>
+      <div className="row" id='landing-row'>
+        <div className="col center-image">
+        <img style={{width: 550, height: 550, alignSelf: 'start' }}src={zodiac} alt="zodiac" />
+        </div>
+    
+        <div className="col" id='landing-pitch'>
+          
+          <h1 id='landing-text'>Are you ready to discover what is written in your stars? </h1>
+          {/* <img style={{width: 200, height:200, alignSelf: 'end' }}src={stars} alt="stars" /> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
